a0a4a54af672b6ca96976886514fdb6068c8efb4
Kelvin van Amstel <k.v.amstel@hotmail.com>
2015-10-28 17:35:55 +0100

    Responsive Menu
    Javascript Menu responsive and one javascript file for two html files

--- Eindopdracht_Kelvin_van Amstel/js/script.js
+++ Eindopdracht_Kelvin_van Amstel/js/script.js
@@ -8,12 +8,22 @@ var radio1 = document.querySelector('input[value="project"]');
 var radio2 = document.querySelector('input[value="stage"]');
 var fieldset1 = document.querySelector('#project');
 var fieldset2 = document.querySelector('#stage');
+var nav = document.querySelector('nav');
+var menu = document.querySelectorAll('h2');
+var secondWidth= window.matchMedia("(max-width: 800px)")
+var thirdWidth= window.matchMedia("(max-width: 600px)")
 
 /////////////////
 //  Functions  //
 /////////////////
-fieldset1.classList.add('invisible');
-fieldset2.classList.add('invisible');
+menu[0].classList.add('invisible');
+
+if (fieldset1 == null){
+    console.log('geen fieldset gevonden');
+} else {
+    fieldset1.classList.add('invisible');
+    fieldset2.classList.add('invisible');
+}
 
 function project() {
     fieldset1.classList.remove('invisible');
@@ -25,6 +35,27 @@ function stage() {
     fieldset1.classList.add('invisible');
 }
 
+//width menu smaller then 801px
+if (secondWidth.matches || thirdWidth.matches) {
+    nav.classList.add('invisible');
+    menu[0].classList.remove('invisible');
+    menu[0].classList.add('h2');
+}
+
+function show(){
+    if(nav.classList == 'invisible') {
+        nav.classList.remove('invisible');
+    } else {
+        nav.classList.add('invisible');
+    }
+}
+
 //  Add Event
-radio1.addEventListener('click', project);
-radio2.addEventListener('click', stage);
+if (fieldset1 == null){
+    console.log('Geen radio button gevonden');
+} else {
+    radio1.addEventListener('click', project);
+    radio2.addEventListener('click', stage);
+}
+
+menu[0].addEventListener('click', show);
